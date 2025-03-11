e1c7ed9d097d56ff4c55c64c67d9c53855be2c71
rifqirafialdy <rifqirafialdy@gmail.com>
2025-03-11 14:34:08 +0700

    fix button link

--- src/components/Portofolio/index.tsx
+++ src/components/Portofolio/index.tsx
@@ -1,49 +1,45 @@
-// src/components/Portofolio.tsx
-
 import { FC, useState } from "react";
-import { DUMMY_PROJECTS } from "@/constants/projectList"; // Make sure the path is correct
+import { DUMMY_PROJECTS } from "@/constants/projectList";
 import { motion } from "framer-motion";
 import Link from "next/link";
 import Image from "next/image";
 
 const Portofolio: FC = () => {
-    const [imgSrc, setImgSrc] = useState("/link.png");
+    const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
 
     return (
         <div className="flex flex-col gap-5">
-            {DUMMY_PROJECTS.map((project, index) => {
-                
-                return (
-                    <div
-                        key={index}
-                        className="flex justify-between items-center pb-7 border-b border-[#C7D0D9]"
-                    >
-                        <div>
-                            <h2 className="font-inter font-normal text-[60px] leading-[72.61px] m-0">
-                                {project.name}
-                            </h2>
-                            <p className="font-neue-montreal font-normal text-[18px] leading-[28.8px] text-[#3C3D3E] m-0">
-                                {project.roles.join(" - ")}
-                            </p>
-                        </div>
-                        
-                        <Link href={project.link} target="_blank">
-                            <motion.div
-                                onHoverStart={() => setImgSrc("/black-link.png")}
-                                onHoverEnd={() => setImgSrc("/link.png")}
-                                className="cursor-pointer"
-                            >
-                                <Image
-                                    src={imgSrc}
-                                    alt="project link"
-                                    width={24}
-                                    height={24}
-                                />
-                            </motion.div>
-                        </Link>
+            {DUMMY_PROJECTS.map((project, index) => (
+                <div
+                    key={index}
+                    className="flex justify-between items-center pb-7 border-b border-[#C7D0D9]"
+                >
+                    <div>
+                        <h2 className="font-inter font-normal text-[60px] leading-[72.61px] m-0">
+                            {project.name}
+                        </h2>
+                        <p className="font-neue-montreal font-normal text-[18px] leading-[28.8px] text-[#3C3D3E] m-0">
+                            {project.roles.join(" - ")}
+                        </p>
                     </div>
-                );
-            })}
+                    
+                    <Link href={project.link} target="_blank">
+                        <motion.div
+                            whileHover={{ scale: 1.1 }}
+                            className="cursor-pointer"
+                            onMouseEnter={() => setHoveredIndex(index)}
+                            onMouseLeave={() => setHoveredIndex(null)}
+                        >
+                            <Image
+                                src={hoveredIndex === index ? "/black-link.png" : "/link.png"}
+                                alt="project link"
+                                width={60}
+                                height={60}
+                            />
+                        </motion.div>
+                    </Link>
+                </div>
+            ))}
         </div>
     );
 };
